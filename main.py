fdc06ac9c38032dc1f25a97d7ac4da255813ea73
Konstantin Kuts <konstantinkuts@Konstantins-MBP.home>
2024-11-30 15:24:12 +0500

    small changes to UI

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/settings')
+def contacts():
+    return render_template('settings.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
